44cef8d04abb68fba99c87d6949321025b522053
codeliling <maximuslee@126.com>
2017-07-18 17:46:49 +0800

    增加阿里云上传接口

--- src/main/java/com/jianma/xtdm/HomeController.java
+++ src/main/java/com/jianma/xtdm/HomeController.java
@@ -3,7 +3,9 @@ package com.jianma.xtdm;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +16,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
 
 /**
  * Handles requests for the application home page.
@@ -43,5 +50,43 @@ public class HomeController {
 	@RequestMapping(value = "/error", method = RequestMethod.GET)
 	public String error(Locale locale, Model model) {
 		return "error";
+	}
+	
+	@RequestMapping(value = "/uploadKey", method = RequestMethod.GET)
+	public @ResponseBody  Map<String, String> uploadKey(Locale locale, Model model) {
+		String endpoint = "oss-cn-hangzhou.aliyuncs.com";
+        String accessId = "LTAI6sQldcdFoZQt";
+        String accessKey = "zl4ywIXQbPhml9LRhwxMsO5w776Ys9";
+        String bucket = "xtdm";
+        String dir = "xtdm_";
+        String host = "http://" + bucket + "." + endpoint;
+        OSSClient client = new OSSClient(endpoint, accessId, accessKey);
+        try { 	
+        	long expireTime = 30;
+        	long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = client.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = client.calculatePostSignature(postPolicy);
+            
+            Map<String, String> respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            //respMap.put("expire", formatISO8601Date(expiration));
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            return respMap;
+            
+        } catch (Exception e) {
+            
+            return null;
+        }
 	}
 }
