ecf472852310e734f631699a87cbf7ea34c7cd86
codeliling <maximuslee@126.com>
2017-08-01 11:27:14 +0800

    代码优化

--- src/main/webapp/resources/js/project/login.js
+++ src/main/webapp/resources/js/project/login.js
@@ -1,5 +1,51 @@
-$(document).ready(function(){
-	$("#submitBtn").click(function(){
-		location.href = "index.html";
+$(document).ready(function() {
+
+	
+
+	$("#usernameLabel").addClass('active');
+	$("#passwordLabel").addClass('active');
+
+	$("#submitBtn").click(function() {
+
+		let
+		username = $("#username").val();
+		let
+		password = $("#password").val();
+
+		let
+		requestData = {
+			"email" : username,
+			"password" : password
+		};
+
+		$.ajax({
+
+			type : "POST",
+			url : "../user/ajaxLogin",
+			contentType : 'application/json',
+			dataType : "json",
+			data : JSON.stringify(requestData),
+			beforeSend : function() {
+				$("#circleProgress").show();
+			},
+			success : function(data) {
+				if (data.resultCode == 200) {
+					location.href = "index.html";
+				} else {
+					$("#loginMsg").html(data.message);
+					$("#circleProgress").hide();
+				}
+			},
+			statusCode : {
+				404 : function() {
+					$("#circleProgress").hide();
+					Materialize.toast('没有加载到相应页面!', 4000);
+				},
+				500 : function() {
+					$("#circleProgress").hide();
+					Materialize.toast('服务器内部错误!', 4000);
+				}
+			}
+		});
 	});
 });
